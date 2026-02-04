a39abd971b2da83cd0a9ef68367076f62e18168d
AbdouazizDEV <abdouaziz@example.com>
2026-02-04 10:09:51 +0000

    Réorganisation de la liste des speakers selon l'ordre prioritaire demandé
    - Réorganisation des 12 premiers speakers selon l'ordre spécifié
    - Mise à jour des rôles (Serigne Mboup: UNCCIAS et Maire de Kaolack)
    - Ajout des images manquantes pour les speakers
    - Suppression de l'import inutilisé

--- src/services/speakersService.js
+++ src/services/speakersService.js
@@ -1,7 +1,6 @@
 import orpheeKinssImage from '../assets/images/Speakers/Orphée initiateur du projet.jpeg';
 import bertinMampakaImage from '../assets/images/Speakers/Bertin Mampaka.jpeg';
 import christianLamoulineImage from '../assets/images/Speakers/Christian Lamouline.jpeg';
-import stevenRoyezImage from '../assets/images/Speakers/Steven Royez.jpeg';
 import WillaneImage from '../assets/images/Speakers/Willane.jpeg';
 import AliouneSarrImage from '../assets/images/Speakers/Alioune Sarr ancien ministre.jpeg';
 import ElhadjDialloImage from '../assets/images/Speakers/ Elhadj DIALLO.jpeg';
@@ -23,183 +22,63 @@ import BiranDioufImage from '../assets/images/Speakers/Biran Diouf.jpeg';
 import CheikhAliouneBeyeImage from '../assets/images/Speakers/Cheikh Alioune BEYE.jpeg';
 import IbrahimaDiedhiouImage from '../assets/images/Speakers/Ibrahima Diedhiou.jpeg';
 import AlHassaneNiangImage from '../assets/images/Speakers/Al Hassane NiANG.jpeg';
+import HalewijnEnabelImage from '../assets/images/Speakers/HALEWIJN ENABEL.jpeg';
+import AbdoulayeLyImage from '../assets/images/Speakers/Abdoulaye Ly.jpeg';
+import SteveRoyezImage from '../assets/images/Speakers/Steve Royez.jpeg';
+import ElHadjMoussaDialloImage from '../assets/images/Speakers/El Hadj Moussa Diallo.jpeg';
+import KalvinSoiresseNjallImage from '../assets/images/Speakers/Kalvin Soiresse Njall.jpeg';
 /**
  * Service contenant les données des speakers du Forum
  */
 export const SPEAKERS_DATA = [
+  // Ordre prioritaire selon la demande du client
   {
     id: 1,
-    name: "Orphée Kinss",
-    title: "Mr",
-    role: "Initiateur du Forum",
-    country: "Belgique",
-    bio: "Fondateur et initiateur du Forum des Territoires",
-    image: orpheeKinssImage
-  },
-  {
-    id: 2,
-    name: "Christian Lamouline",
-    title: "Mr",
-    role: "President de BRULOCALIS et  Bourgmestre de Berchem-Sainte-Agathe",
-    country: "Belgique",
-    bio: "Haut représentant institutionnel belge",
-    image: christianLamoulineImage
-  },
-  {
-    id: 3,
     name: "Bertin Mampaka",
     title: "Mr",
-    role: "Président du parlement bruxellois",
+    role: "Président du parlement bruxellois et Sénateur belge",
     country: "Belgique",
-    bio: "Haut représentant institutionnel belge",
+    bio: "Président du parlement bruxellois et Sénateur belge",
     image: bertinMampakaImage
   },
   {
+    id: 2,
+    name: "Oury Baillo Diallo",
+    title: "Mr",
+    role: "Président UAEL Sénégal",
+    country: "Sénégal",
+    bio: "Président de l'Union des Associations d'Élus Locaux",
+    image: OuryBailloDialloImage
+  },
+  {
+    id: 3,
+    name: "Christian Lamouline",
+    title: "Mr",
+    role: "Président de BRULOCALIS et Bourgmestre de Berchem-Sainte-Agathe",
+    country: "Belgique",
+    bio: "Président de BRULOCALIS et Bourgmestre de Berchem-Sainte-Agathe",
+    image: christianLamoulineImage
+  },
+  {
     id: 4,
-    name: "Steven Royez",
+    name: "Serigne Mboup",
+    title: "Mr",
+    role: "Président de l'UNCCIAS et Maire de Kaolack",
+    country: "Sénégal",
+    bio: "Président de l'UNCCIAS et Maire de Kaolack",
+    image: SerigneMboupImage
+  },
+  {
+    id: 5,
+    name: "Steve Royez",
     title: "Mr",
     role: "Bourgmestre de Lobbes",
     country: "Belgique",
     bio: "Président d'IGRETEC et Administrateur de l'Union des villes et communes de Wallonie, asbl",
-    image: stevenRoyezImage
-  },
-  {
-    id: 5,
-    name: "Diarra Sow",
-    title: "Mme",
-    role: "Directrice Générale OLAC",
-    country: "Sénégal",
-    bio: "Directrice Générale de l'Organisation pour le Logement et l'Aménagement des Collectivités",
-    image: DiarraSowImage
+    image: SteveRoyezImage
   },
   {
     id: 6,
-    name: "Abdoulaye Wilane",
-    title: "Mr",
-    role: "Président Conseil Régional Kaffrine",
-    country: "Sénégal",
-    bio: "Président du Conseil Régional de Kaffrine",
-    image: WillaneImage
-  },
-  {
-      id: 7,
-    name: "Serigne Mboup",
-    title: "Mr",
-    role: "Président Chambre de commerce de Kaolack",
-    country: "Sénégal",
-    bio: "Président de la Chambre de commerce de Kaolack",
-    image: SerigneMboupImage
-  },
-  {
-    id: 8,
-    name: "Biran Diouf",
-    title: "Mr",
-    role: "Directeur des Opérations de l'ADEPME",
-    country: "Sénégal",
-    bio: "Directeur des Opérations de l'Agence de Développement et d'Encadrement des Petites et Moyennes Entreprises",
-    image: BiraneDioufImage
-  },
-  {
-    id: 9,
-    name: "Elhadj DIALLO  ",
-    title: "Mr",
-    role: "Député au Parlement de la Région de Bruxelles-Capitale  ",
-    country: "Belgique",
-    bio: "Sénateur de Belgique",
-    image: ElhadjDialloImage
-  },
-  {
-    id: 10,
-    name: "André FLAHAUT",
-    title: "Mr",
-    role: "Ministre d’État, Ministre de la Défense de 1999 à 2008",
-    country: "Sénégal",
-    bio: " président de la Chambre des représentants (2010-2014) et occupé des fonctions ministérielles en Fédération Wallonie-Bruxelles",
-    image: AndreFlhautImage
-  },
-  {
-    id: 11,
-    name: "Marie Claire Mvumb",
-    title: "Mme",
-    role: "Sénatrice belge",
-    country: "Belgique",
-    bio: "sénatrice belge",
-    image: MarieClaireMvumbImage
-  },
-  {
-    id: 12,
-    name: "Nabil Messaoudi",
-    title: "Mr",
-    role: "Échevin des sports, Familles, Cohésion sociale, Jeunesse, Jumelages et Coopération internationale Ixelles.",
-    country: "Belgique",
-    bio: null,
-    image: NabilMessaoudiImage
-  },
-  {
-    id: 13,
-    name: "Cheikh Ibra Ndiaye",
-    title: "Mr",
-    role: "Maire de Thilmakha",
-    country: "Sénégal",
-    bio: "Maire de la commune de Thilmakha",
-    image: CheikhIbraNdiayeImage
-  },
-  {
-    id: 14,
-    name: "Alioune Pouye",
-    title: "Mr",
-    role: "Maire de Sebikotane",
-    country: "Sénégal",
-    bio: "Maire de la commune de Sebikotane",
-    image: AliounePouyeImage
-  },
-  {
-    id: 15,
-    name: "Abdoulaye Ly",
-    title: "Mr",
-    role: "Ancien Directeur des investissements",
-    country: "Sénégal",
-    bio: "Ancien Directeur des investissements",
-    image: null
-  },
-  {
-    id: 16,
-    name: "Al Hassane NiANG",
-    title: "Mr",
-    role: "Directeur du Groupe GOPA, Ancien DG de B&s Europe,Expert en Gouvernance publique et reformes Institutionnelles",
-    country: "Sénégal",
-    bio: "Parrains de la 1 ère édition",
-    image: AlHassaneNiangImage
-  },
-  {
-    id: 17,
-    name: "Jamil Thiam",
-    title: "Mr",
-    role: "Président de l'Association des Sénégalais de la Belgique",
-    country: "Belgique",
-    bio: "Président de l'Association des Sénégalais de la Belgique",
-    image: null
-  },
-  {
-    id: 18,
-    name: "Ndeye Awa Ndiaye",
-    title: "Mme",
-    role: "",
-    country: "Sénégal",
-    bio: "",
-    image: NdeyeAwaNdiayeImage
-  },
-  {
-    id: 19,
-    name: "Hakewijn Timmerman",
-    title: "Mr",
-    role: "Chef de projet Système Alimentaire Durable, de l'entrepreneuriat et du changement climatique",
-    country: "Belgique",
-    bio: "Expert en systèmes alimentaires durables et entrepreneuriat",
-    image: null
-  },
-  {
-    id: 20,
     name: "Alioune Sarr",
     title: "Mr",
     role: "Ancien Ministre du Tourisme au Sénégal",
@@ -208,7 +87,161 @@ export const SPEAKERS_DATA = [
     image: AliouneSarrImage
   },
   {
+    id: 7,
+    name: "Kalvin Soiresse Njall",
+    title: "Mr",
+    role: "Député et Parrain 1ère édition",
+    country: "Belgique",
+    bio: "Député et Parrain 1ère édition",
+    image: KalvinSoiresseNjallImage
+  },
+  {
+    id: 8,
+    name: "Al Hassane NiANG",
+    title: "Mr",
+    role: "Directeur du Groupe GOPA, Ancien DG de B&s Europe, Expert en Gouvernance publique et reformes Institutionnelles",
+    country: "Sénégal",
+    bio: "Parrains de la 1 ère édition",
+    image: AlHassaneNiangImage
+  },
+  {
+    id: 9,
+    name: "HALEWIJN ENABEL",
+    title: "Mr",
+    role: "",
+    country: "Belgique",
+    bio: "",
+    image: HalewijnEnabelImage
+  },
+  {
+    id: 10,
+    name: "Diarra Sow",
+    title: "Mme",
+    role: "Directrice Générale OLAC",
+    country: "Sénégal",
+    bio: "Directrice Générale de l'Organisation pour le Logement et l'Aménagement des Collectivités",
+    image: DiarraSowImage
+  },
+  {
+    id: 11,
+    name: "Marie Claire Mvumb",
+    title: "Mme",
+    role: "Sénatrice belge",
+    country: "Belgique",
+    bio: "Sénatrice belge",
+    image: MarieClaireMvumbImage
+  },
+  {
+    id: 12,
+    name: "Abdoulaye Ly",
+    title: "Mr",
+    role: "Ancien Directeur des investissements",
+    country: "Sénégal",
+    bio: "Ancien Directeur des investissements",
+    image: AbdoulayeLyImage
+  },
+  // Autres speakers (ordre restant)
+  {
+    id: 13,
+    name: "Orphée Kinss",
+    title: "Mr",
+    role: "Initiateur du Forum",
+    country: "Belgique",
+    bio: "Fondateur et initiateur du Forum des Territoires",
+    image: orpheeKinssImage
+  },
+  {
+    id: 14,
+    name: "Abdoulaye Wilane",
+    title: "Mr",
+    role: "Président Conseil Régional Kaffrine",
+    country: "Sénégal",
+    bio: "Président du Conseil Régional de Kaffrine",
+    image: WillaneImage
+  },
+  {
+    id: 15,
+    name: "Biran Diouf",
+    title: "Mr",
+    role: "Directeur des Opérations de l'ADEPME",
+    country: "Sénégal",
+    bio: "Directeur des Opérations de l'Agence de Développement et d'Encadrement des Petites et Moyennes Entreprises",
+    image: BiraneDioufImage
+  },
+  {
+    id: 16,
+    name: "Elhadj DIALLO",
+    title: "Mr",
+    role: "Député au Parlement de la Région de Bruxelles-Capitale",
+    country: "Belgique",
+    bio: "Sénateur de Belgique",
+    image: ElhadjDialloImage
+  },
+  {
+    id: 17,
+    name: "André FLAHAUT",
+    title: "Mr",
+    role: "Ministre d'État, Ministre de la Défense de 1999 à 2008",
+    country: "Sénégal",
+    bio: "Président de la Chambre des représentants (2010-2014) et occupé des fonctions ministérielles en Fédération Wallonie-Bruxelles",
+    image: AndreFlhautImage
+  },
+  {
+    id: 18,
+    name: "Nabil Messaoudi",
+    title: "Mr",
+    role: "Échevin des sports, Familles, Cohésion sociale, Jeunesse, Jumelages et Coopération internationale Ixelles.",
+    country: "Belgique",
+    bio: null,
+    image: NabilMessaoudiImage
+  },
+  {
+    id: 19,
+    name: "Cheikh Ibra Ndiaye",
+    title: "Mr",
+    role: "Maire de Thilmakha",
+    country: "Sénégal",
+    bio: "Maire de la commune de Thilmakha",
+    image: CheikhIbraNdiayeImage
+  },
+  {
+    id: 20,
+    name: "Alioune Pouye",
+    title: "Mr",
+    role: "Maire de Sebikotane",
+    country: "Sénégal",
+    bio: "Maire de la commune de Sebikotane",
+    image: AliounePouyeImage
+  },
+  {
     id: 21,
+    name: "Jamil Thiam",
+    title: "Mr",
+    role: "Président de l'Association des Sénégalais de la Belgique",
+    country: "Belgique",
+    bio: "Président de l'Association des Sénégalais de la Belgique",
+    image: null
+  },
+  {
+    id: 22,
+    name: "Ndeye Awa Ndiaye",
+    title: "Mme",
+    role: "",
+    country: "Sénégal",
+    bio: "",
+    image: NdeyeAwaNdiayeImage
+  },
+  {
+    id: 23,
+    name: "Hakewijn Timmerman",
+    title: "Mr",
+    role: "Chef de projet Système Alimentaire Durable, de l'entrepreneuriat et du changement climatique",
+    country: "Belgique",
+    bio: "Expert en systèmes alimentaires durables et entrepreneuriat",
+    image: null
+  },
+  {
+    id: 24,
     name: "Mamadou Oury Barry",
     title: "Mr",
     role: "Secrétaire Général de l'UAL",
@@ -217,16 +250,7 @@ export const SPEAKERS_DATA = [
     image: null
   },
   {
-    id: 22,
-    name: "Oury Baillo Diallo",
-    title: "Mr",
-    role: "Président UAEL",
-    country: "Sénégal",
-    bio: "Président de l'Union des Associations d'Élus Locaux",
-    image: OuryBailloDialloImage
-  },
-  {
-    id: 23,
+    id: 25,
     name: "Idrissa Diop",
     title: "Mr",
     role: "Secrétaire Général de l'UEAL",
@@ -235,7 +259,7 @@ export const SPEAKERS_DATA = [
     image: IdrissaDiopImage
   },
   {
-    id: 24,
+    id: 26,
     name: "Sultan DIOP",
     title: "Mr",
     role: "PDG de NOGA MINE SARL",
@@ -244,7 +268,7 @@ export const SPEAKERS_DATA = [
     image: SultanDiopImage
   },
   {
-    id: 25,
+    id: 27,
     name: "Aicha Bacha",
     title: "Mme",
     role: "Présidente fondatrice de l'ECDA",
@@ -253,7 +277,7 @@ export const SPEAKERS_DATA = [
     image: AichaBachaImage
   },
   {
-    id: 26,
+    id: 28,
     name: "Somaya Korkzine",
     title: "Mme",
     role: "Presidente Asbl SO GO UP",
@@ -262,7 +286,7 @@ export const SPEAKERS_DATA = [
     image: SomayaKorkzineImage
   },
   {
-    id: 27,
+    id: 29,
     name: "Biran Diouf",
     title: "Mr",
     role: "",
@@ -271,22 +295,31 @@ export const SPEAKERS_DATA = [
     image: BiranDioufImage
   },
   {
-    id: 28,
+    id: 30,
     name: "Cheikh Alioune BEYE",
     title: "Mr",
-    role: "Maire de Sicap Mbao et PCA  du Conseil d’orientation des pôles urbains de Diamniadio et Lac Rose",
+    role: "Maire de Sicap Mbao et PCA du Conseil d'orientation des pôles urbains de Diamniadio et Lac Rose",
     country: "Sénégal",
     bio: "",
     image: CheikhAliouneBeyeImage
   },
   {
-    id: 29,
+    id: 31,
     name: "Ibrahima Diedhiou",
     title: "Mr",
-    role: " maire de Bona",
+    role: "Maire de Bona",
     country: "Sénégal",
     bio: "",
     image: IbrahimaDiedhiouImage
+  },
+  {
+    id: 32,
+    name: "El Hadj Moussa Diallo",
+    title: "Mr",
+    role: "Député et Sénateur",
+    country: "Sénégal",
+    bio: "",
+    image: ElHadjMoussaDialloImage
   }
 ];
 
